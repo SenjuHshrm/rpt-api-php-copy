d30efd5ebd4b6f58317a0f32fdbce07a0476161e
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-06-20 16:09:44 +0800

    Code fix, added function for getting encoder name

--- app/Http/Controllers/GetLandFaas.php
+++ app/Http/Controllers/GetLandFaas.php
@@ -8,7 +8,19 @@ use Illuminate\Support\Facades\DB;
 class GetLandFaas extends Controller
 {
     public function getInfo(Request $request) {
-        $res = "CALL get_land_faas(".$request['id'].")";
-        return json_encode($res);
+        $res = DB::select("CALL get_land_faas(".$request['id'].")");
+        $obj = $res[0];
+        $obj->encoder_id = $this->getEncoder($obj->encoder_id);
+        return json_encode($obj);
+    }
+
+    private function getEncoder($id) {
+        $q = DB::select("CALL get_user_credentials(".$id.")");
+        if($q[0]->middle_name == '') {
+            $res = $q[0]->first_name . ' ' .$q[0]->last_name;
+        } else {
+            $res = $q[0]->first_name . ' ' . substr($q[0]->middle_name, 0) . '. ' .$q[0]->last_name;
+        }
+        return $res;
     }
 }
