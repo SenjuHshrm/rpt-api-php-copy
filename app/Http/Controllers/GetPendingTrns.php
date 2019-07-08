ec78fa209fce5a9c019f36040e6b64a1882dbe53
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-07-08 12:01:03 +0800

    Functions for getting pending transactions

--- app/Http/Controllers/GetPendingTrns.php
+++ app/Http/Controllers/GetPendingTrns.php
@@ -8,14 +8,14 @@ use Illuminate\Support\Facades\DB;
 class GetPendingTrns extends Controller
 {
     public function getPendingSegregation(Request $req) {
-
+			return json_encode(DB::select("CALL get_pending_segregated_lands('".$req['name']."')"));
 		}
 
 		public function getPendingConsolidation(Request $req) {
-
+			return json_encode(DB::select("CALL get_pending_consolidated_lands('".$req['name']."')"));
 		}
 
 		public function getPendingSubdivision(Request $req) {
-
+			return json_encode(DB::select("CALL get_pending_subdivided_lands('".$req['name']."')"));
 		}
 }
